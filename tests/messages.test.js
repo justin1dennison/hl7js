56a0d142cda67b48a920b4157b1b6234533af8bd
Justin Dennison <justin1dennison@gmail.com>
2019-12-30 15:03:11 -0500

    created MSH message segment and started working on the message

---
+++ tests/messages.test.js
@@ -0,0 +1,156 @@
+const test = require('tape')
+
+test('subfields can be retained when required', t => {
+  t.pass()
+  t.end()
+})
+
+test('segments can be added to existing message', t => {
+  t.pass()
+  t.end()
+})
+
+test('fields can be added to existing segments', t => {
+  t.pass()
+  t.end()
+})
+
+test('control characters can be customized using second argument', t => {
+  t.pass()
+  t.end()
+})
+
+test('message can be converted to string', t => {
+  t.pass()
+  t.end()
+})
+
+test('toString method throws exception when message empty', t => {
+  t.pass()
+  t.end()
+})
+
+test('fields and subfields can be set properly', t => {
+  t.pass()
+  t.end()
+})
+
+test('fields cna be separated by custom character', t => {
+  t.pass()
+  t.end()
+})
+
+test('components of a field can be separated by custom character', t => {
+  t.pass()
+  t.end()
+})
+
+test('subcomponents can be separated by a custom character', t => {
+  t.pass()
+  t.end()
+})
+
+test('segments can be added from message', t => {
+  t.pass()
+  t.end()
+})
+
+test('segment can be removed from message', t => {
+  t.pass()
+  t.end()
+})
+
+test('segments can be removed from message using index', t => {
+  t.pass()
+  t.end()
+})
+
+test('segments can be removed by name', t => {
+  t.pass()
+  t.end()
+})
+
+test('a new segment can be inserted between two existing segments', t => {
+  t.pass()
+  t.end()
+})
+
+test('it should not be possible to insert segment beyond last index', t => {
+  t.pass()
+  t.end()
+})
+
+test('a segment can be overwitten', t => {
+  t.pass()
+  t.end()
+})
+
+test('same segment type can be added multiple times', t => {
+  t.pass()
+  t.end()
+})
+
+test('field separator can be changed after message creation', t => {
+  t.pass()
+  t.end()
+})
+
+test('a message can be constructed from a string', t => {
+  t.pass()
+  t.end()
+})
+
+test('a message can be converted to a string', t => {
+  t.pass()
+  t.end()
+})
+
+test('a segment can be retrieved as a string', t => {
+  t.pass()
+  t.end()
+})
+
+test('an exception will be thrown in invalid string', t => {
+  t.pass()
+  t.end()
+})
+
+test('segment ending bar can be omitted', t => {
+  t.pass()
+  t.end()
+})
+
+test('segment index can be retrieved from a message', t => {
+  t.pass()
+  t.end()
+})
+
+test('message type can be checked', t => {
+  t.pass()
+  t.end()
+})
+
+test('segment id can be reset on demand', t => {
+  t.pass()
+  t.end()
+})
+
+test('segment index autoincrement can be avoided', t => {
+  t.pass()
+  t.end()
+})
+
+test('a segments presence can be checked', t => {
+  t.pass()
+  t.end()
+})
+
+test('first of the given segment name can be easily obtained using a helper method', t => {
+  t.pass()
+  t.end()
+})
+
+test('message can be verified as empty', t => {
+  t.pass()
+  t.end()
+})
